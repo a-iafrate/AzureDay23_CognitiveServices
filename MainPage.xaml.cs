d1354bf61cd7dbf4438737e09f577732ad5952b9
alessio iafrate <alessioiafrate@hotmail.com>
2023-06-01 18:33:50 +0200

    test code

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using Azure.AI.Vision.Common.Options;
 using Azure;
+using Azure.AI.Vision.ImageAnalysis;
+using Microsoft.Maui.Controls;
+using Azure.AI.Vision.Common.Input;
 
 namespace AzureDay23_CognitiveServices;
 
@@ -22,14 +25,56 @@ public partial class MainPage : ContentPage
 			CounterBtn.Text = $"Clicked {count} times";
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+
+        RemoveBackground();
+
+    }
 
 	private void RemoveBackground()
 	{
 
         var serviceOptions = new VisionServiceOptions(
-    Environment.GetEnvironmentVariable("VISION_ENDPOINT"),
-    new AzureKeyCredential(Environment.GetEnvironmentVariable("VISION_KEY")));
+    "https://azureday23cogser.cognitiveservices.azure.com",
+    new AzureKeyCredential("76d79cf0f3d849f78718b2e8470c8f80"));
+
+        using var imageSource = VisionSource.FromUrl(
+    new Uri("https://learn.microsoft.com/azure/cognitive-services/computer-vision/media/quickstarts/presentation.png"));
+
+        var analysisOptions = new ImageAnalysisOptions()
+        {
+            SegmentationMode = ImageSegmentationMode.BackgroundRemoval
+        };
+
+        using var analyzer = new ImageAnalyzer(serviceOptions, imageSource, analysisOptions);
+
+        var result = analyzer.Analyze();
+
+        if (result.Reason == ImageAnalysisResultReason.Analyzed)
+        {
+            using var segmentationResult = result.SegmentationResult;
+
+            var imageBuffer = segmentationResult.ImageBuffer;
+            Console.WriteLine($" Segmentation result:");
+            Console.WriteLine($"   Output image buffer size (bytes) = {imageBuffer.Length}");
+            Console.WriteLine($"   Output image height = {segmentationResult.ImageHeight}");
+            Console.WriteLine($"   Output image width = {segmentationResult.ImageWidth}");
+
+            string outputImageFile = "output.png";
+            using (var fs = new FileStream(outputImageFile, FileMode.Create))
+            {
+                fs.Write(imageBuffer.Span);
+            }
+            Console.WriteLine($"   File {outputImageFile} written to disk");
+        }
+        else
+        {
+            var errorDetails = ImageAnalysisErrorDetails.FromResult(result);
+            Console.WriteLine(" Analysis failed.");
+            Console.WriteLine($"   Error reason : {errorDetails.Reason}");
+            Console.WriteLine($"   Error code : {errorDetails.ErrorCode}");
+            Console.WriteLine($"   Error message: {errorDetails.Message}");
+            Console.WriteLine(" Did you set the computer vision endpoint and key?");
+        }
     }
 }
 
